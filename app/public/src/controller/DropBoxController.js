c46814f6a1d5ececad95e33c97cdba10b95aaf4b
brunoBrian <bruninho_brian@hotmail.com>
2020-03-02 23:57:53 -0300

    feat: selecionado arquivos segurando ctrl e shift

--- app/public/src/controller/DropBoxController.js
+++ app/public/src/controller/DropBoxController.js
@@ -314,7 +314,47 @@ class DropBoxController {
       <div class="name text-center">${file.name}</div>
     `;
 
+    this.initEventsLi(li);
+
     return li;
+  }
+
+  initEventsLi(li) {
+    li.addEventListener('click', e => {
+
+      if(e.shiftKey) {
+        let firstLi = this.listFilesEl.querySelector('.selected');
+
+        if(firstLi) {
+          let indexStart;
+          let indexEnd;
+          let lis = li.parentElement.childNodes;
+
+          lis.forEach((el, index) => {
+            if(firstLi === el) indexStart = index;
+            if(li === el) indexEnd = index;
+          });
+
+          let index = [indexStart, indexEnd].sort();
+
+          lis.forEach((el, idx) => {
+            if(idx >= index[0] && idx <= index[1]) {
+              el.classList.add('selected');
+            }
+          });
+
+          return true;
+        }
+      }
+
+      if(!e.ctrlKey) {
+        this.listFilesEl.querySelectorAll('li.selected').forEach(el => {
+          el.classList.remove('selected');
+        });
+      }
+
+      li.classList.toggle('selected');
+    });
   }
   
   readFiles() {
@@ -328,8 +368,6 @@ class DropBoxController {
         let data = snapItem.val();
 
         this.listFilesEl.appendChild(this.getFileView(data, key));
-        
-        console.log(key, data)
       });
     });
   }
